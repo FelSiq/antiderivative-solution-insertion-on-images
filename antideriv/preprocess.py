82fe3ebcac24eef9821dbf2c3b4b05f063ea3d4a
felsiq <felipe.siqueira@usp.br>
2019-05-29 16:11:40 -0300

    Updated preprocess module

--- antideriv/preprocess.py
+++ antideriv/preprocess.py
@@ -4,7 +4,7 @@ import typing as t
 import matplotlib.pyplot as plt
 import skimage.filters
 import skimage.color
-import skimage.feature
+import skimage.segmentation
 import numpy as np
 
 
@@ -25,24 +25,28 @@ class Preprocessor:
         """
         self.img_preprocessed = None  # type: t.Optional[np.ndarray]
 
-    def preprocess(self, img: np.ndarray) -> np.ndarray:
+    def preprocess(self, img: np.ndarray, plot: bool = False) -> np.ndarray:
         """Preprocess the input image.
 
         The procedures applied to the image are:
             1. RGB to Grayscale
-            2.
+            2. Otsu threshold
             3. Image empty border crop
         """
         self.img_preprocessed = skimage.color.rgb2gray(img)
 
-        """
-        threshold = np.percentile(self.img_preprocessed, 0.5)
-        threshold = self.img_preprocessed.mean()
-        self.img_preprocessed = 255 * (self.img_preprocessed <= threshold)
-        """
-        self.img_preprocessed = skimage.feature.canny(self.img_preprocessed)
+        if plot:
+            plt.subplot(121)
+            plt.imshow(self.img_preprocessed, cmap="gray")
 
-        plt.imshow(self.img_preprocessed, cmap="gray")
-        plt.show()
+        self.img_preprocessed = skimage.filters.sobel(self.img_preprocessed)
+
+        threshold = skimage.filters.threshold_otsu(self.img_preprocessed)
+        self.img_preprocessed = 255 * (self.img_preprocessed > threshold)
+
+        if plot:
+            plt.subplot(122)
+            plt.imshow(self.img_preprocessed, cmap="gray")
+            plt.show()
 
         return self.img_preprocessed
