d7c8202d0f6b8351fbbaca4a6ba6ac74d1ffd1cc
FelSiq <felipe.siqueira@usp.br>
2019-06-03 19:15:06 -0300

    Build experimental cnn architectures

--- symbol-recognition/symbol_recog.py
+++ symbol-recognition/symbol_recog.py
@@ -7,7 +7,8 @@ import re
 import numpy as np
 import tensorflow
 import skimage
-from keras.layers import Dropout, Dense, Conv2D, MaxPooling2D, Flatten
+from keras.layers import Dropout, Dense, Conv2D
+from keras.layers import MaxPooling2D, Flatten, BatchNormalization
 import keras.models
 import keras.utils
 import sklearn.model_selection
@@ -30,11 +31,114 @@ class CNNModel:
 
         self._ARCH_NAME, self._ARCH_CALLABLE = zip(*aux)
 
+        self.arch_num = len(self._ARCH_NAME)
+
     def architecture_1(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=5, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=64, kernel_size=5, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_2(self):
         """Predefined CNN architecture 01."""
         self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_3(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=48, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_4(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=48, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=96, activation="relu"))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_5(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=96, activation="relu"))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_6(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=5, activation="relu"))
+        self.model.add(Conv2D(filters=48, kernel_size=5, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Conv2D(filters=64, kernel_size=5, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_7(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.1, seed=self.random_seed))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.1, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=96, activation="relu"))
+        self.model.add(Dropout(0.1, seed=self.random_seed))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_8(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.1, seed=self.random_seed))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.1, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dropout(0.1, seed=self.random_seed))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_9(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Dropout(0.2, seed=self.random_seed))
 
         self.model.add(Flatten())
@@ -42,12 +146,121 @@ class CNNModel:
         self.model.add(Dropout(0.2, seed=self.random_seed))
         self.model.add(Dense(units=self.num_classes, activation="softmax"))
 
-    def architecture_2(self):
-        """Predefined CNN architecture 02."""
-        self.model.add(Dense(units=16, activation="relu"))
-        self.model.add(Dropout(0.1, seed=self.random_seed))
-        self.model.add(Dense(units=32, activation="relu"))
-        self.model.add(Dropout(0.1, seed=self.random_seed))
+    def architecture_10(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_11(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+        self.model.add(Conv2D(filters=48, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_12(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=32, kernel_size=5, activation="relu"))
+        self.model.add(BatchNormalization())
+
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=64, kernel_size=5, activation="relu"))
+        self.model.add(BatchNormalization())
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_13(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=32, kernel_size=5, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=64, kernel_size=5, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_14(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=32, kernel_size=5, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Conv2D(filters=64, kernel_size=5, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.4, seed=self.random_seed))
+        self.model.add(Dense(units=self.num_classes, activation="softmax"))
+
+    def architecture_15(self):
+        """Predefined CNN architecture 01."""
+        self.model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+        self.model.add(Conv2D(filters=48, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+        self.model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+        self.model.add(MaxPooling2D(pool_size=(2, 2)))
+        self.model.add(Dropout(0.2, seed=self.random_seed))
+
+        self.model.add(Flatten())
+        self.model.add(Dense(units=128, activation="relu"))
+        self.model.add(Dropout(0.2, seed=self.random_seed))
         self.model.add(Dense(units=self.num_classes, activation="softmax"))
 
     def init_architecture(self, architecture_id: int, loss: str,
@@ -87,6 +300,11 @@ class CNNModel:
 
         results = []
 
+        es = keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            min_delta=0.01,
+            patience=5)
+
         for ind_train, ind_test in kfold.split(X, y):
             X_train, y_train = X[ind_train, :], y[ind_train]
             X_test, y_test = X[ind_test, :], y[ind_test]
@@ -99,16 +317,26 @@ class CNNModel:
                 y=y_train,
                 validation_split=validation_split,
                 epochs=epochs,
-                batch_size=batch_size)
+                batch_size=batch_size,
+                callbacks=[es],
+                verbose=0)
 
             res = self.model.evaluate(X_test, y_test)
             results.append(res)
 
         return np.array(results)
 
-    def freeze_architecture(self) -> None:
+    def freeze_architecture(self, subpath: t.Optional[str] = None) -> None:
         """Save the validated architecture."""
-        self.model.save("{}_{}.h5".format("model", str(self.architecture_id)))
+        prefix = "model"
+
+        if subpath:
+            prefix = os.path.join(subpath, prefix)
+
+        if not os.path.exists(subpath):
+            os.makedirs(subpath)
+
+        self.model.save("{}_{}.h5".format(prefix, str(self.architecture_id)))
 
 
 def get_data(filepath: str) -> t.Tuple[np.ndarray, np.ndarray]:
@@ -130,7 +358,7 @@ def get_data(filepath: str) -> t.Tuple[np.ndarray, np.ndarray]:
         X += new_insts
         y += len(new_insts) * [class_name]
 
-    X, y = np.array(X) // 255, np.array(y)
+    X, y = np.array(X).astype(np.uint8) // 255, np.array(y)
 
     return X.reshape(*X.shape, 1), y
 
@@ -144,7 +372,10 @@ if __name__ == "__main__":
 
     results = {}
 
-    for architecture_id in [1]:
+    for architecture_id in np.arange(6, model.arch_num+1):
+        print("Started training with architecture {}..."
+              "".format(architecture_id))
+
         model.init_architecture(
             architecture_id=architecture_id,
             loss="categorical_crossentropy",
@@ -155,15 +386,18 @@ if __name__ == "__main__":
             X=X,
             y=y,
             n_splits=10,
-            validation_split=0.15,
-            epochs=12,
+            validation_split=0.1,
+            epochs=30,
             batch_size=32)
 
         results[architecture_id] = cur_score
+        print("Architecture {} mean results:".format(architecture_id),
+              cur_score.mean(axis=0))
 
+        model.freeze_architecture(subpath="./frozen_models")
+
+    print("Final results")
     for architecture_id in results:
         cur_results = results[architecture_id]
         print("Architecture {} mean results:".format(architecture_id),
               cur_results.mean(axis=0))
-
-    # model.freeze_architecture()
